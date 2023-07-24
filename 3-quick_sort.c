ce44f6759f642ec1559d74bade2bc14ae41b6a67
SouaOui <soumiaouzat@gmail.com>
2023-07-24 18:17:40 +0100

    still debug

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,44 +1,44 @@
-#include "sort.h"
-/**
- *
- *
- *
- */
-void quick_sort_recursion(int *array, int start, int end)
-{
-    int position_index;
+#include <stdio.h>
 
-    position_index = partition_array(array, start, end);
-    quick_sort_recursion(array, start, position_index - 1);
-    quick_sort_recursion(array, position_index + 1, end);
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-int partition_array(int *array, int start, int end)
+size_t partition(int *array, size_t low, size_t high)
 {
-    
+    int pivot = array[high];
+    size_t i = low - 1;
+    size_t j;
+
+    for (j = low; j < high; j++)
+    {
+        if (array[j] <= pivot)
+        {
+            i++;
+            swap(&array[i], &array[j]);
+        }
+    }
+    swap(&array[i + 1], &array[high]);
+    printf("value of index pivot is %ld\n", i  + 1);
+    return i + 1;
+}
+
+void quick_sort_recursion(int *array, size_t low, size_t high)
+{
+    if (low < high)
+    {
+        size_t pivot_index = partition(array, low, high);
+        printf("Low = %ld \t High = %ld\n", low, high);
+        quick_sort_recursion(array, low, pivot_index - 1);
+        printf("Low = %ld \t High = %ld\n \t Pivot Index %ld\n", low, high, pivot_index);
+        quick_sort_recursion(array, pivot_index + 1, high);
+        printf("Low = %ld \t High = %ld\n", low, high);
+    }
 }
 void quick_sort(int *array, size_t size)
 {
-    size_t i = 0;
-    size_t j = -1;
-    size_t pivot = size - 1;
-    int tmp;
-
-    if (size < 2)
-        return;
-    while (i <= pivot)
-    {
-        if (array[i] <= array[pivot])
-        {
-            j++;
-            if (i != j)
-            {
-                tmp = array[j];
-                array[j] = array[i];
-                array[i] = tmp;
-                print_array(array, size);
-            }
-        }
-        i++;
-    }
+    quick_sort_recursion(array, 0, size - 1);
 }
