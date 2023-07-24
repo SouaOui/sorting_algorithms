454db49076d7621ae28ceed086f8c1fdab13d3de
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-24 22:13:56 +0000

    Task4 fix betty and bug in output

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,51 +1,46 @@
 #include "sort.h"
-
+/**
+ * shell_sort - function
+ * @array: pointer to array
+ * @size: index of the start
+ * Return: Nothing
+ */
 void shell_sort(int *array, size_t size)
 {
 	size_t gap = 1, i = 0;
-	int j = 0, ii = 0;
-	int tmp;
+	int j = 0, ii = 0, tmp;
 
+	if (!array || size < 2)
+		return;
 	while (gap <= size)
 	{
 		if (gap * 3 + 1 > size)
 			break;
 		gap = gap * 3 + 1;
 	}
-	for (gap = gap; gap > 1; gap = (gap - 1) / 3)
+	for (i = 0, gap = gap; gap > 1 && i + gap < size; gap = (gap - 1) / 3, i++)
 	{
-		for (i = 0; i + gap < size; i++)
+		if (array[i] > array[i + gap])
 		{
-			if (array[i] > array[i + gap])
+			for (j = i + gap, ii = i; j >= 0 && ii >= 0 && array[ii] > array[j];
+				 j = j - gap, ii = ii - gap)
 			{
-				for (j = i + gap, ii = i; j >= 0 && ii >= 0; j = j - gap, ii = ii - gap)
-				{
-					if (array[ii] > array[j])
-					{
-						tmp = array[j];
-						array[j] = array[i];
-						array[i] = tmp;
-					}
-				}
+				tmp = array[j];
+				array[j] = array[ii];
+				array[ii] = tmp;
 			}
 		}
 		print_array(array, size);
 	}
-	j = 0;
-	ii = 0;
-	i = 0;
 	for (i = 1; i < size; i++)
 	{
 		if (array[i - 1] > array[i])
 		{
-			for (j = i - 1, ii = i; j >= 0; j--, ii--)
+			for (j = i - 1, ii = i; j >= 0 && array[ii] < array[j]; j--, ii--)
 			{
-				if (array[ii] < array[j])
-				{
-					tmp = array[ii];
-					array[ii] = array[j];
-					array[j] = tmp;
-				}
+				tmp = array[ii];
+				array[ii] = array[j];
+				array[j] = tmp;
 			}
 		}
 	}
