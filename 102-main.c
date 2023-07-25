a1872a9897ac5accbf7d82df0ce66ac3bf70a87d
Mounssif BOUHLAOUI <cv.bouhlaoui@gmail.com>
2023-07-25 19:39:57 +0000

    Task6 fix:output last number is missing

--- 102-main.c
+++ 102-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int array[] = {8,1};
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
@@ -21,17 +21,17 @@ int main(void)
 }
 
 /*
-*
-*loop throughinput  array get max number
-*	create count array[max+1] initlise to 0
-*		loop through input array
-*			each value in input array
-*			use it as index in count array to inc++
-*		loop through count array
-*			add the previos value to the next index
-*		create new array same lenght as input
-*			loop through input
-*				each value copy it to :
-*					new_array[count_array[array[i]]-1]
-*		copy new array to input array
-*/
+ *
+ *loop throughinput  array get max number
+ *	create count array[max+1] initlise to 0
+ *		loop through input array
+ *			each value in input array
+ *			use it as index in count array to inc++
+ *		loop through count array
+ *			add the previos value to the next index
+ *		create new array same lenght as input
+ *			loop through input
+ *				each value copy it to :
+ *					new_array[count_array[array[i]]-1]
+ *		copy new array to input array
+ */
